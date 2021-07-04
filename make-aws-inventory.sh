85d793d727a747547526df80257a79b1675d5867
Fabian Lee <flee999@hotmail.com>
2021-07-04 11:05:48 -0400

    need terraform 14

--- make-aws-inventory.sh
+++ make-aws-inventory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-terraform14 output | grep ec2_web_instance_ip_address | awk '{print $3}' | tr -d '"'
+terraform output | grep ec2_web_instance_ip_address | awk '{print $3}' | tr -d '"'

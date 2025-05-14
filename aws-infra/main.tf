5d6680c89311d588c845f341cd3fea535041108e
Fabian Lee <flee999@hotmail.com>
2025-05-14 06:43:30 -0400

    removed deprecated template_file resource that was commented out

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -68,16 +68,6 @@ resource "aws_instance" "wgserver" {
   depends_on = [ aws_internet_gateway.my_gateway ]
 }
 
-/*
-data "template_file" "default" {
-  template = file("${path.module}/startup.sh")
-  vars = {
-    foo = "bar"
-  }
-}
-*/
-
-
 # private apache web server
 resource "aws_instance" "web" {
   #name = not supported
@@ -94,7 +84,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [ aws_security_group.web_sg.id ]
 
   # coming from template
-  #user_data = data.template_file.default.rendered
   user_data = local.template_data
 
   # directly from file
